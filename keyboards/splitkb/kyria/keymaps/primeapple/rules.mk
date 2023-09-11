f2a2aad486e9ca13a1744e8b970cf2d0f7b8fb15
primeapple <toni.mueller.web@mailbox.org>
2023-09-11 16:42:53 +0200

    Removed tapdance

--- keyboards/splitkb/kyria/keymaps/primeapple/rules.mk
+++ keyboards/splitkb/kyria/keymaps/primeapple/rules.mk
@@ -3,4 +3,4 @@ OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-TAP_DANCE_ENABLE = yes      # Enable tap dance
+TAP_DANCE_ENABLE = no      # Enable tap dance
